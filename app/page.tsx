ce5ab087cb78917dce5e1d78be139a78125ab0c7
chris-lawbrokr <chris@lawbrokr.com>
2026-08-25 11:32:35 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <main className="flex flex-col items-center gap-4">
+        <Button>Click me</Button>
+      </main>
+    </div>
+  );
+}
